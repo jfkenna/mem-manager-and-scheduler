a2edf3c4ce2a9d36c0453c29c12689a4d2a2c73f
jfkenna-unimelb <jfkenna@student.unimelb.edu.au>
2020-05-30 07:51:26 +1000

    read fixed

--- mem.c
+++ mem.c
@@ -4,6 +4,7 @@
 
 //defines
 #define MAX_NUM_PROCESSES 1000
+
 //process information struct
 typedef struct Process{
     int time_arrived;
@@ -12,27 +13,33 @@ typedef struct Process{
     int job_time;
 } process;
 
+//process helper function
+void print_process(process target_process){
+    printf("%d %d %d %d\n", target_process.time_arrived, target_process.process_id, 
+    target_process.memory_size_req, target_process.job_time);
+}
 
 //hidden to actual assignment, stores incoming data with having to constantly do IO
-process* create_incoming_process_array(char* filename){
+process* create_incoming_process_array(char* filename, int* new_len){
     process* process_array = calloc(MAX_NUM_PROCESSES, sizeof(process));
+    if (process_array == NULL){
+        printf("realloc failed!!! program should prob exit so add code for it");
+    }
     FILE* file = fopen(filename, "r");
     
-    //file open failed
+    //if file open failed
     if (file == NULL){
         printf("null file, returning...");
-        return NULL;
+        return 0;
     }
-    //read processes file into buffer, BUGGED
+
+    //read processes file into buffer
     int offset = 0;
-    while (fscanf(file,"%d %d %d %d\r\n", &process_array[offset].time_arrived, 
+    for (;fscanf(file,"%d %d %d %d\r\n", &process_array[offset].time_arrived, 
             &process_array[offset].process_id, &process_array[offset].memory_size_req, 
-            &process_array[offset].job_time) != EOF){
-        offset += 1;
-        printf("offset: %d, ", offset);
-        printf("%d %d %d %d\n", process_array[offset].time_arrived, process_array[offset].process_id,
-         process_array[offset].memory_size_req, process_array[offset].job_time);  
+            &process_array[offset].job_time) == 4;offset += 1){
     }
+    *new_len = offset;
     return process_array;
 }
 
@@ -45,11 +52,17 @@ int main(int argc, char** argv){
 
     //read data
     printf("attempting to read file: \"%s\"\n", argv[1]);
-    process* process_array = create_incoming_process_array(argv[1]);
-    if (process_array == NULL){
+    int* process_ar_len = malloc(sizeof(int));
+    process* process_array = create_incoming_process_array(argv[1], process_ar_len);
+
+    //handle error
+    if (*process_ar_len == 0){
         printf("error during file read... returning...");
         return 1;
+    }else{
+        for (int i = 0; i < *process_ar_len; i++){
+            print_process(process_array[i]);
+        }
     }
-    printf("end");
     return 0;
 }
