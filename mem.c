1f550775e5a650db2463f852f841f2b0d663c046
jfkenna-unimelb <jfkenna@student.unimelb.edu.au>
2020-05-30 09:02:01 +1000

    basic scheduler implemented, logic and mem errors not yet fixed

--- mem.c
+++ mem.c
@@ -5,44 +5,149 @@
 //defines
 #define MAX_NUM_PROCESSES 1000
 
+//memory config
+#define MEM_UNLIMITED 1
+#define MEM_SWAPPING 2
+#define MEM_VIRTUAL 3
+#define MEM_CUSTOM 4
+
+//scheduler config
+#define SCHEDULER_FCFS 1
+#define SCHEDULER_RR 2
+#define SCHEDULER_CUSTOM 3
+
+//***********************************************************************************************
 //process information struct
-typedef struct Process{
+typedef struct process{
     int time_arrived;
     int process_id;
     int memory_size_req;
     int job_time;
 } process;
 
-//process helper function
-void print_process(process target_process){
-    printf("%d %d %d %d\n", target_process.time_arrived, target_process.process_id, 
-    target_process.memory_size_req, target_process.job_time);
+//***********************************************************************************************
+//queue and helper functions
+
+typedef struct Queue_node queue_node;
+struct Queue_node{
+    queue_node* next;
+    queue_node* prev;
+    process* value;
+};
+
+typedef struct Process_queue process_queue; 
+struct Process_queue{
+    queue_node* front;
+    queue_node* back;
+    int len;
+};
+
+void queue_enqueue(process_queue* queue, process* new_process){
+    //create new node
+    queue_node* new_node = malloc(sizeof(queue_node));
+    new_node->value = new_process;
+    new_node->prev = NULL;
+    if (queue->len != 0){
+        new_node->next = queue->back;
+    }else{
+        new_node->next = NULL;
+    }
+
+    //update queue to reflect new addition
+    queue->back->prev = new_node;
+    queue->back = new_node; 
 }
 
-//hidden to actual assignment, stores incoming data with having to constantly do IO
-process* create_incoming_process_array(char* filename, int* new_len){
-    process* process_array = calloc(MAX_NUM_PROCESSES, sizeof(process));
-    if (process_array == NULL){
-        printf("realloc failed!!! program should prob exit so add code for it");
+process* queue_dequeue(process_queue* queue){
+    process* return_val = queue->front->value;
+    queue->front->prev->next = NULL;
+    queue->front = queue->front->prev;
+    if (queue->len == 1){
+        queue->back = NULL;
     }
+    queue->len -= 1;
+    return return_val;
+}
+
+//***********************************************************************************************
+//printing functions
+void print_process(process* target_process){
+    printf("%d %d %d %d\n", target_process->time_arrived, target_process->process_id, 
+    target_process->memory_size_req, target_process->job_time);
+}
+
+void print_queue(process_queue* queue){
+    printf("printing from the back of the queue");
+    queue_node* cur = queue->back;
+    while(cur != NULL){
+        print_process(cur->value);
+        cur = cur->next;
+    }
+}
+
+
+
+//***********************************************************************************************
+//hidden to actual assignment, stores incoming data with having to constantly do IO
+process_queue* load_processes(char* filename){
+    //open file
     FILE* file = fopen(filename, "r");
-    
+
     //if file open failed
     if (file == NULL){
         printf("null file, returning...");
-        return 0;
+        return NULL;
     }
-
-    //read processes file into buffer
-    int offset = 0;
-    for (;fscanf(file,"%d %d %d %d\r\n", &process_array[offset].time_arrived, 
-            &process_array[offset].process_id, &process_array[offset].memory_size_req, 
-            &process_array[offset].job_time) == 4;offset += 1){
+    
+    //load processes
+    process_queue* incoming_process_queue = malloc(sizeof(process_queue));
+    for (process* new_process = malloc(sizeof(process)); fscanf(file,"%d %d %d %d\r\n", &new_process->time_arrived, 
+            &new_process->process_id, &new_process->memory_size_req, 
+            &new_process->job_time) == 4; queue_enqueue(incoming_process_queue, new_process)){
+        //create a new process to write to and enqueue
+        new_process = malloc(sizeof(process));
     }
-    *new_len = offset;
-    return process_array;
+    return incoming_process_queue;
 }
 
+//***********************************************************************************************
+//dummy function so code compiles, replace later
+int load_memory(process* cur_process, int memory_manager_type){
+    printf("loaded memory...");
+    return 0;
+}
+
+//***********************************************************************************************
+//first come first served scheduler
+void first_come_first_served(process_queue* incoming_process_queue, int memory_manager_type){
+    int global_time = 0;
+
+    process* cur_process = queue_dequeue(incoming_process_queue);
+    while (cur_process != NULL){
+        //simulate waiting for new process
+        if (cur_process->time_arrived > global_time){
+            global_time += cur_process->time_arrived - global_time;
+        }
+
+        //print generic process start info
+        printf("current_time is %d , program running, id %d, remaining time %d", global_time, cur_process->process_id, cur_process->job_time);
+        
+        //load required pages
+        int load_memory_cost = 0;
+        if (memory_manager_type != MEM_UNLIMITED){
+            load_memory_cost = load_memory(cur_process, memory_manager_type);
+        }
+        
+        //simulate time spent loading pages and running job
+        global_time += load_memory_cost + cur_process->job_time;
+
+        //get next enqueued process
+        cur_process = queue_dequeue(incoming_process_queue);
+    }
+}
+
+
+//***********************************************************************************************
 int main(int argc, char** argv){
     //check correct args are passed
     if (argc != 6){
@@ -52,17 +157,16 @@ int main(int argc, char** argv){
 
     //read data
     printf("attempting to read file: \"%s\"\n", argv[1]);
-    int* process_ar_len = malloc(sizeof(int));
-    process* process_array = create_incoming_process_array(argv[1], process_ar_len);
+    process_queue* incoming_process_queue = load_processes(argv[1]);
+    print_queue(incoming_process_queue);
 
     //handle error
-    if (*process_ar_len == 0){
+    if (incoming_process_queue->len == 0){
         printf("error during file read... returning...");
         return 1;
-    }else{
-        for (int i = 0; i < *process_ar_len; i++){
-            print_process(process_array[i]);
-        }
     }
+
+    //run basic scheduler with unlimited memory
+    first_come_first_served(incoming_process_queue, MEM_UNLIMITED);
     return 0;
 }
